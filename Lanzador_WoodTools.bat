56b2806e6e23be24b6d905ae2a4009dc70a39f45
woodtoolsmarketing <woodtoolsmarketing@gmail.com>
2026-02-24 09:10:49 -0300

    actualización-code-24/2/2026

---
+++ Lanzador_WoodTools.bat
@@ -0,0 +1,28 @@
+@echo off
+color 0A
+title Actualizador WoodTools
+cd /d "%~dp0"
+
+echo Buscando actualizaciones en GitHub...
+git fetch origin prueba
+
+FOR /F "tokens=*" %%a in ('git rev-parse HEAD') do SET LOCAL_VER=%%a
+FOR /F "tokens=*" %%a in ('git rev-parse origin/prueba') do SET REMOTE_VER=%%a
+
+if "%LOCAL_VER%"=="%REMOTE_VER%" (
+    echo.
+    echo ===================================
+    echo    PROGRAMA SIN ACTUALIZACIONES
+    echo ===================================
+    timeout /t 2 > NUL
+) else (
+    echo.
+    echo Descargando nuevos cambios...
+    git pull origin prueba
+    echo Actualizacion completada.
+    timeout /t 2 > NUL
+)
+
+:: Abre el programa y cierra esta ventana negra
+start pythonw interfaz.py
+exit
